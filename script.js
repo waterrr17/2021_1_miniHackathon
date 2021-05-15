01cec0dd9f2f11ce90002772c1501f764e85b221
Miju <mj0904meeju@naver.com>
2021-05-15 09:56:05 +0900

    css, js 분리

---
+++ script.js
@@ -0,0 +1,42 @@
+var count = 0;
+var time=[];
+var countList=[];
+
+function Set() {
+	clearInterval(time);
+ 	count=60*Number(document.getElementById("settingMinutes").value)+Number(document.getElementById("settingSeconds").value);
+
+ 	var lists=document.getElementsByClassName("countdown");
+	var peopleNum=Number(document.getElementById('people').value);
+ 	for (var i=0; i<peopleNum; i++){
+ 		if (count%60<10)
+			lists[i].innerHTML= parseInt(count/60)+" : 0"+(count%60);
+		else
+			lists[i].innerHTML= parseInt(count/60)+" : "+(count%60);
+		countList[i]=count;
+	}
+}
+
+function Play(n){
+	time[n] = setInterval(function(){Count(n);},1000);
+}
+function Stop(n){
+	clearInterval(time[n]);
+}
+
+function Count(n) {
+	countList[n]=countList[n]-1;
+	inCount=countList[n];
+	var lists=document.getElementsByClassName("countdown");
+	if (inCount%60<10){
+		lists[n].innerHTML= parseInt(inCount/60)+" : 0"+(inCount%60);
+	}
+	else{
+		lists[n].innerHTML= parseInt(inCount/60)+" : "+(inCount%60);
+	}
+	if(inCount <= 0) { 
+		clearInterval(time[n]);
+		alert("시간이 완료되었습니다.")
+		// 태양 넣기
+	}
+}
